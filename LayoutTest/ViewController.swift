1346d756f5c72c595a71491107e28b7b4fa5d0b8
Maxim Polezhaev <maxim.polezhaev97@gmail.com>
2019-09-14 19:43:41 +0300

    Initial Commit

---
+++ LayoutTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutTest
+//
+//  Created by mbp on 14/09/2019.
+//  Copyright © 2019 mbp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
